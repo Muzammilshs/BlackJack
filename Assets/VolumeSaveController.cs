b2c228f15c78684f9e17fd2e0bcc27a711f235bd
Moiz Khan <moikzzz02@gmail.com>
2024-11-12 17:20:08 +0500

    New

--- Assets/VolumeSaveController.cs
+++ Assets/VolumeSaveController.cs
@@ -7,27 +7,58 @@ public class VolumeSaveController : MonoBehaviour
     public TextMeshProUGUI volumeText;  
     private const string VolumePrefKey = "VolumeLevel";
 
-    void Start()
-    {
-        // Load saved volume or set default to 0 if not set
-        volumeSlider.value = PlayerPrefs.GetFloat(VolumePrefKey, 0);
+    // void Start()
+    // {
+    //     // Load saved volume or set default to 0 if not set
+    //     volumeSlider.value = PlayerPrefs.GetFloat(VolumePrefKey, 0);
 
         
-        //UpdateVolume();
+    //     //UpdateVolume();
         
-        volumeSlider.onValueChanged.AddListener(delegate { UpdateAndSaveVolume(); });
-    }
+    //     volumeSlider.onValueChanged.AddListener(delegate { UpdateAndSaveVolume(); });
+    // }
 
-    void UpdateAndSaveVolume()
-    {
+    // void UpdateAndSaveVolume()
+    // {
         
-        AudioListener.volume = volumeSlider.value / 5f;
+    //     AudioListener.volume = volumeSlider.value / 5f;
 
        
-        volumeText.text = volumeSlider.value.ToString("0.000");
+    //     volumeText.text = volumeSlider.value.ToString("0.000");
 
         
-        PlayerPrefs.SetFloat(VolumePrefKey, volumeSlider.value);
+    //     PlayerPrefs.SetFloat(VolumePrefKey, volumeSlider.value);
+    //     PlayerPrefs.Save();
+    // }
+      
+      void Start()
+      {
+        float savedVolume = PlayerPrefs.GetFloat(VolumePrefKey, 0);
+        volumeSlider.value = savedVolume;
+
+        UpdateVolume(savedVolume);
+
+
+        volumeSlider.onValueChanged.AddListener(delegate {UpdateAndSaveVolume();});
+
+      }
+
+
+      void UpdateAndSaveVolume()
+      {
+        float currentVolume = volumeSlider.value;
+        UpdateVolume(currentVolume);
+
+        
+        PlayerPrefs.SetFloat(VolumePrefKey, currentVolume);
         PlayerPrefs.Save();
-    }
+      }
+
+      void UpdateVolume(float volume)
+      {
+        AudioListener.volume = volume / 5f;
+        volumeText.text = volume.ToString("0.000");
+
+      }
+
 }
